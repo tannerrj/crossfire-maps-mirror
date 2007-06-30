78fc0a05c8b4c023814183e7449bd37fb22108e6
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-06-30 22:00:14 +0000

    Fix damage getting really big, more fun effects.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps/branches/1.x@6734 282e977c-c81d-0410-88c4-b93c2d0d6712

--- python/items/weapon_occidental_mages.py
+++ python/items/weapon_occidental_mages.py
@@ -6,32 +6,29 @@ ac = Crossfire.WhoIsActivator()
 r  = random.random()
 
 if (r <= 0.01):
-	ac.Write("Your weapon suddenly seems lighter!")
-	me.Dam = me.Dam + 10
-	me.BeenApplied=0
-	me.Identified=0
+	if me.Weight == me.Archetype.Clone.Weight:
+		ac.Write("Your weapon suddenly seems lighter!")
+		me.Weight = 9 * me.Archetype.Clone.Weight / 10
+		me.Dam = me.Archetype.Clone.Dam
+		me.LastSP = me.Archetype.Clone.LastSP
 elif (r <= 0.02):
-	ac.Write("Your weapon suddenly seems darker!")
-	me.Dam = me.Dam - 10
-	me.BeenApplied=0
-	me.Identified=0
+	if me.Dam == me.Archetype.Clone.Dam:
+		ac.Write("Your weapon suddenly seems sharper!")
+		me.Dam = 2 * me.Archetype.Clone.Dam
+		me.Weight = me.Archetype.Clone.Weight
+		me.LastSP = me.Archetype.Clone.LastSP
 elif (r <= 0.03):
-	ac.Write("Your weapon suddenly seems lighter!")
-	me.Dam = me.Dam + 10
-	me.Identified=0
-	me.BeenApplied=0
+	if me.LastSP == me.Archetype.Clone.LastSP:
+		ac.Write("Your weapon suddenly seems easier to handle!")
+		me.LastSP = me.Archetype.Clone.LastSP + 1
+		me.Dam = me.Archetype.Clone.Dam
+		me.Weight = me.Archetype.Clone.Weight
 elif (r <= 0.04):
 	ac.Write("Your weapon suddenly seems colder!")
 	me.AttackType = Crossfire.AttackType.COLD + Crossfire.AttackType.PHYSICAL
-	me.Identified=0
-	me.BeenApplied=0
 elif (r <= 0.05):
 	ac.Write("Your weapon suddenly seems warmer!")
 	me.AttackType=Crossfire.AttackType.FIRE + Crossfire.AttackType.PHYSICAL
-	me.Identified=0
-	me.BeenApplied=0
 elif (r <= 0.06):
 	ac.Write("Your weapon suddenly emits sparks!")
 	me.AttackType=Crossfire.AttackType.ELECTRICITY + Crossfire.AttackType.PHYSICAL
-	me.Identified=0
-	me.BeenApplied=0
