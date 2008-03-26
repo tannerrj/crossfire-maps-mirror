c4a7f2cc5cf0dc8a9f3096ea2865c55a49442603
gros <gros@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-03-26 18:15:15 +0000

    Introduced Part I of the Brewery Quest, replacing Newbie Tower.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps@8638 282e977c-c81d-0410-88c4-b93c2d0d6712

--- trunk/python/maps/scorn/towers/mad_mage/mad_mage_user.py
+++ trunk/python/maps/scorn/towers/mad_mage/mad_mage_user.py
@@ -6,11 +6,18 @@ whoisother = Crossfire.WhoIsOther()
 parms = Crossfire.WhatIsMessage()
 
 if parms=="give":
+    Crossfire.Log(Crossfire.LogDebug, "Object given: %s" %(whoisother.Name))
     key = whoisother.ReadKey("mad_mage_marker")
+    Crossfire.Log(Crossfire.LogDebug, "Object key  : %s" %(key))
     if key:
-        file = open(os.path.join(Crossfire.DataDirectory(), Crossfire.MapDirectory(), 'python/maps/scorn/towers/mad_mage',key), "r")
+        mypath = os.path.join(Crossfire.DataDirectory(), Crossfire.MapDirectory(), 'python/maps/scorn/towers/mad_mage',key)
+        Crossfire.Log(Crossfire.LogDebug, "Object path : %s" %(mypath))
+        file = open(mypath, "r")
         for line in file:
             whoami.Say(line)
         file.close()
+        if (key == 'brewery_letter'):
+            whoisother.Name = "Milking machine blueprints"
+            whoisother.WriteKey("mad_mage_marker", "milking_blueprints")
     else:
         whoami.Say("Yes?")
