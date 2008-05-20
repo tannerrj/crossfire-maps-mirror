22da146226e2363e96ed37ca2b8933165002d169
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-05-20 21:25:32 +0000

    Some fun with plugins.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps@9125 282e977c-c81d-0410-88c4-b93c2d0d6712

---
+++ trunk/test/cfanim.py
@@ -0,0 +1,4 @@
+import Crossfire
+
+#Crossfire.WhoAmI().Say('message is %s'%Crossfire.WhatIsMessage())
+Crossfire.WhoAmI().Event(Crossfire.WhoAmI(), Crossfire.WhoAmI(), Crossfire.WhatIsMessage(), 0)
