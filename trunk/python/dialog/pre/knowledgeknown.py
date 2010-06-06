b3fcafa471ef033a094dabd213b567f2dbd5f67a
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-06-06 15:14:56 +0000

    Fix comments.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps@13370 282e977c-c81d-0410-88c4-b93c2d0d6712

--- trunk/python/dialog/pre/knowledgeknown.py
+++ trunk/python/dialog/pre/knowledgeknown.py
@@ -4,13 +4,12 @@
 # The following code runs when a dialog has a pre rule of 'knowledgeknown'
 # The syntax is ["knowledgeknown", "knowledge"]
 # To deliver a True verdict, the player must have the knowledge represented
-# by "knowledge" (specific format depending on the format).
+# by "knowledge" (specific format depending on the type of knowledge).
 #
 ## DIALOGCHECK
 ## MINARGS 1
 ## MAXARGS 1
 ## .*
-## \*
 ## ENDDIALOGCHECK
 
 knowledge = args[0]
