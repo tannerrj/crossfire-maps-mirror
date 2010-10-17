aaba0b13956fc2a1043802784eadd66f81d50cf2
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-10-17 16:07:49 +0000

    White zone of Darcap's Manor.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps@14007 282e977c-c81d-0410-88c4-b93c2d0d6712

--- trunk/python/maps/darcap/manor.py
+++ trunk/python/maps/darcap/manor.py
@@ -1,9 +1,12 @@
+import random
 # -*- coding: utf-8 -*-
 # Script handling various things for Darcap Manor.
 #
 # It is used:
 # - in the blue zone, to count how many kobolds are on the map
 # - for the potion, to explode walls in the treasure room
+# - in the brown zone, to prevent Kaptel's death or trigger the opening of the exit
+# - in the white zone, to check if the player knows how many of a certain jewel there are
 
 import Crossfire
 
@@ -22,6 +25,7 @@ def blue_count_kobolds():
     return count
 
 def blue_check():
+    '''Player triggered the lever, check if correct number of kobolds. '''
 
     if blue_count_kobolds() != 4:
         Crossfire.SetReturnValue(1)
@@ -29,6 +33,7 @@ def blue_check():
         return
 
 def potion_find_wall(map, x, y, name):
+    '''Find the wall of a given archetype at the specified location.'''
     item = map.ObjectAt(x, y)
     while item:
         if item.ArchName == name:
@@ -95,9 +100,80 @@ def kaptel_death():
     who.HP = who.MaxHP / 2
     Crossfire.SetReturnValue(1)
 
+def challenge_correct_reply(count, msg):
+    if count == 0:
+        return msg == '0' or msg == 'none'
+    return count == int(msg)
+
+def check_arch(map, x, y, arch):
+    below = map.ObjectAt(x, y)
+    while below:
+        if below.ArchName == arch:
+            return 1
+        below = below.Above
+    
+    return 0
+
+def white_challenge():
+    '''
+    Say handler for the final white challenge.
+    Player must know how many jewel piles of a certain kind were on the map, else failure.
+    '''
+
+    msg = Crossfire.WhatIsMessage()
+    ear = Crossfire.WhoAmI()
+    status = ear.ReadKey('challenge')
+
+    if status == 'done':
+        ear.Say('You already replied!')
+        return
+
+    if status == '':
+        if msg == 'ready':
+            ear.WriteKey('challenge', 'ready', 1)
+            ear.Say('To get the white key, reply to the following question:')
+
+            archs = [ 'gem', 'amethyst', 'emerald', 'ruby', 'sapphire', 'pearl' ]
+            names = [ 'diamonds', 'amethysts', 'emeralds', 'rubies', 'sapphires', 'pearls' ]
+            choice = random.randint(0, 5)
+
+            location = random.randint(1, 3)
+            if location == 1:
+                positions = [ [8, 2], [10, 7], [10, 9], [28, 18] ]
+                where = 'behind windows'
+            elif location == 2:
+                positions = [ [10, 7], [10, 9], [2, 23], [16, 21], [8, 2], [24, 6], [23, 7], [25, 7], [19, 26], [20, 26], [19, 28] ]
+                where = 'behind grates'
+            else:
+                positions = [ [8, 2], [10, 7], [10, 9], [2, 23], [16, 21], [8, 2], [24, 6], [23, 7], [25, 7], [28, 18], [19, 26], [20, 26], [19, 28] ]
+                where = 'in this dungeon'
+
+            count = 0
+            for pos in positions:
+                count += check_arch(ear.Map, pos[0], pos[1], archs[choice])
+
+            ear.Say('How many piles of %s did you see %s?'%(names[choice],where))
+
+            # keep reply in  the ear, easier
+            ear.WriteKey('reply', str(count), 1)
+        return
+
+    # check answer
+
+    ear.WriteKey('challenge', 'done')
+
+    count = int(ear.ReadKey('reply'))
+    if challenge_correct_reply(count, msg):
+        ear.Map.TriggerConnected(60, 1)
+        ear.Say('Correct!')
+    else:
+        ear.Say('Sorry, that is not the correct reply. Please try again later.')
+
 if Crossfire.ScriptParameters() == 'blue':
     blue_check()
 elif Crossfire.ScriptParameters() == 'potion':
     potion_check()
 elif Crossfire.ScriptParameters() == 'kaptel':
     kaptel_death()
+elif Crossfire.ScriptParameters() == 'white':
+    white_challenge()
