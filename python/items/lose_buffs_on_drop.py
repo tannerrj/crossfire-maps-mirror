f7c5f34c40bdeb8a65519ff7ffbfa5f7b1a27bd2
silvernexus <silvernexus@282e977c-c81d-0410-88c4-b93c2d0d6712>
2020-08-07 04:13:52 +0000

    Add scripts for \(weapon\) of Souls. Depends on r21252 and r21253.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps/trunk@21254 282e977c-c81d-0410-88c4-b93c2d0d6712

---
+++ python/items/lose_buffs_on_drop.py
@@ -0,0 +1,26 @@
+import Crossfire
+
+me = Crossfire.WhoAmI()
+ac = Crossfire.WhoIsActivator()
+
+me.Str = me.Archetype.Clone.Str
+me.Dex = me.Archetype.Clone.Dex
+me.Con = me.Archetype.Clone.Con
+me.Int = me.Archetype.Clone.Int
+me.Pow = me.Archetype.Clone.Pow
+me.Wis = me.Archetype.Clone.Wis
+me.Cha = me.Archetype.Clone.Cha
+me.HP = me.Archetype.Clone.HP
+me.SP = me.Archetype.Clone.SP
+me.Grace = me.Archetype.Clone.Grace
+me.LastSP = me.Archetype.Clone.LastSP
+me.WC = me.Archetype.Clone.WC
+me.AC = me.Archetype.Clone.AC
+me.Dam = me.Archetype.Clone.Dam
+me.Weight = me.Archetype.Clone.Weight
+me.AttackType = me.Archetype.Clone.AttackType
+me.Food = me.Archetype.Clone.Food
+# Experience should be affected before Item Power, since it affects that field
+me.AddExp(-me.PermExp)
+me.ItemPower = me.Archetype.Clone.ItemPower
+ac.Write("The "+me.Name+" shudders and looks almost like a normal weapon again.")
