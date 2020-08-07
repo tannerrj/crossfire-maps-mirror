4baaabc21b68ce3e14cf092d768823ffae7d0160
silvernexus <silvernexus@282e977c-c81d-0410-88c4-b93c2d0d6712>
2020-08-07 00:50:43 +0000

    Add a clause that only applies the curse when wielding, rather than both wielding and unwielding. Also add known_cursed to the affected fields so it shows up as cursed.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/maps/trunk@21251 282e977c-c81d-0410-88c4-b93c2d0d6712

--- python/items/curse_on_apply.py
+++ python/items/curse_on_apply.py
@@ -3,5 +3,8 @@ import Crossfire
 me = Crossfire.WhoAmI()
 ac = Crossfire.WhoIsActivator()
 
-ac.Write("You feel the "+me.Name+" bind to you.")
-me.Cursed = 1
+# Since this checks before equipping actually occurs, Applied will be 0 when we apply
+if me.Applied == 0:
+    ac.Write("You feel the "+me.Name+" bind to you.")
+    me.Cursed = 1
+    me.KnownCursed = 1
