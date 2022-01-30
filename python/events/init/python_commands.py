e0338dc2b2e626530da68e39d64716706101e978
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2022-01-30 09:41:07 +0100

    Register 'autojail' and 'disinfect' as DM-only

--- python/events/init/python_commands.py
+++ python/events/init/python_commands.py
@@ -1,8 +1,8 @@
 import Crossfire
 
-Crossfire.RegisterCommand("autojail", "/python/commands/autojail", 0)
+Crossfire.RegisterCommand("autojail", "/python/commands/autojail", 0, 2)
 Crossfire.RegisterCommand("dip", "/python/commands/dip", 0)
-Crossfire.RegisterCommand("disinfect", "/python/commands/disinfect", 0)
+Crossfire.RegisterCommand("disinfect", "/python/commands/disinfect", 0, 2)
 Crossfire.RegisterCommand("give", "/python/commands/give", 0)
 Crossfire.RegisterCommand("report", "/python/commands/report", 0)
 Crossfire.RegisterCommand("reputation", "/python/commands/reputation", 0)
